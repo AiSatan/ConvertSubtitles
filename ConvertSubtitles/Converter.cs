2ce08c8a196adfb7563ec103324f2860030adec9
Fandus Atipov <aisatan.ne@live.com>
2017-07-12 01:23:25 +0300

    first

---
+++ ConvertSubtitles/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConvertSubtitles
+{
+    public static class Converter
+    {
+        public static void ConverterVttToSrt(string path)
+        {
+            var subs = File.ReadAllLines(path);
+            var newSub = new List<string>();
+            var num = 1;
+            for (var i = 0; i < subs.Length; i++)
+            {
+                if (i < 2)
+                {
+                    continue;
+                }
+
+                if (Regex.IsMatch(subs[i],
+                    "^[0-9]{2}[:]{1}[0-9]{2}[:]{1}[0-9]{2}[.]{1}[0-9]{3} [-]{2}[>]{1} [0-9]{2}[:]{1}[0-9]{2}[:]{1}[0-9]{2}[.]{1}[0-9]{3}$"))
+                {
+                    newSub.Add($"{num}");
+                    num++;
+                    subs[i] = subs[i].Replace(".", ",");
+                }
+                newSub.Add(subs[i]);
+            }
+            File.WriteAllLines(path.Replace(".vtt", ".srt"), newSub);
+        }
+    }
+}
---
+++ ConvertSubtitles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSubtitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Converter.ConverterVttToSrt(args[0]);
+                Console.WriteLine("done!");
+            }
+            else
+            {
+                Console.WriteLine("drop subs to the app");
+            }
+            Console.ReadKey();
+        }
+    }
+}
